bc497f86aeb3e9f4a9f6603145a4acd85f0e609b
ASUS VivoBook <ASUS VivoBook@DESKTOP-LIARBAQ>
2024-02-22 17:38:23 -0600

    Agregar archivos de proyecto.

---
+++ Practica1API/Controllers/equiposController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practica1API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practica1API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetALL")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
+
+            if (listadoEquipo.Count == 0) 
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+
+                    
+        }
+    }
+}
